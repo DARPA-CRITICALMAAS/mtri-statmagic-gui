23eba43d0572b16eddc515320eddc1fd624a1e57
Alex J. Mueller <ajmuelle@mtu.edu>
2023-10-04 10:42:13 -0400

    Fixes another bug in setpath.bash wherein  was one directory too high

--- setpath.bash
+++ setpath.bash
@@ -1,4 +1,4 @@
-SCRIPTDIR="$( cd "$(dirname "${BASH_SOURCE[0]}")/.." &> /dev/null && pwd )"
+SCRIPTDIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd )"
 
 DEFAULT_PATH=~/.local/share/QGIS/QGIS3/profiles/default/python/plugins/
 export QGIS_PLUGINPATH="$DEFAULT_PATH;${SCRIPTDIR}/plugins/"
