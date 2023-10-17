17e561f580fce086c05512c450686bfb243e7ccd
Alex J. Mueller <ajmuelle@mtu.edu>
2023-10-17 11:59:00 -0400

    Moves generic gui code to new file

--- plugins/StatMaGIC/dockwidget.py
+++ plugins/StatMaGIC/dockwidget.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from osgeo import gdal
 
 from .utils import getSelectionAsArray, gdalSave
+from .gui import *
 
 
 class StatMaGICDockWidget(QtWidgets.QDockWidget):
@@ -24,18 +25,19 @@ class StatMaGICDockWidget(QtWidgets.QDockWidget):
         self.tabWidget.setGeometry(QRect(10, 60, 391, 511))
 
         # create tabs
-        mineral_tab = self.addTab("Mineral Assessment")
-        geochemistry_tab = self.addTab("Geo Chemistry")
-        geology_tab = self.addTab("Geology")
+        mineral_tab = addTab(self.tabWidget, "Mineral Assessment")
+        geochemistry_tab = addTab(self.tabWidget, "Geo Chemistry")
+        geology_tab = addTab(self.tabWidget, "Geology")
 
         # who
-        self.nameInput = self.addTextInput(mineral_tab, "Investigator Name")
+        self.nameInput = addTextInput(mineral_tab, "Investigator Name")
 
         # what
-        self.mineralBox = self.addComboBox(mineral_tab, "Mineral of Interest:", ["Aluminum", "Copper", "Silicon"])
-        self.studyAreaBox = self.addComboBox(mineral_tab, "Define Study Area:", ["Draw Rectangle", "Inherit from Layer", "Canvas Extent"])
+        self.mineralBox = addComboBox(mineral_tab, "Mineral of Interest", ["Aluminum", "Copper", "Silicon"])
+        self.studyAreaBox = addComboBox(mineral_tab, "Define Study Area", ["Draw Rectangle", "Inherit from Layer", "Canvas Extent"])
         # select resolution
-        self.xSpinBox, self.ySpinBox = self.addDoubleSpinBox(mineral_tab, "Resolution:", 1280, 720)
+        self.pixelSizeBox = addSpinBox(mineral_tab, "Pixel Size", 10., 5000.)
+        self.addBufferBox = addSpinBox(mineral_tab, "Buffer", 0., 10000.)
         #   data storage calculator
         # outputs : json, template raster, data pointer list (empty)
 
@@ -56,7 +58,7 @@ class StatMaGICDockWidget(QtWidgets.QDockWidget):
 
         self.setWidget(self.dockWidgetContents)
 
-        self.setAllObjectNames()
+        # self.setAllObjectNames()
         pass
 
     def getTab(self, item) -> QtWidgets.QWidget:
@@ -64,80 +66,6 @@ class StatMaGICDockWidget(QtWidgets.QDockWidget):
             return self.tabWidget.widget(item)
         else:
             return self.tabWidget.findChild(QtWidgets.QWidget, item)
-
-    def addTab(self, tabName):
-        # create tab
-        newTab = QtWidgets.QWidget()
-        newTab.setObjectName(tabName)
-
-        # set layout such that each subcomponent is arranged vertically
-        tabLayout = QtWidgets.QVBoxLayout()
-        tabLayout.setSpacing(0)
-        tabLayout.setAlignment(QtCore.Qt.AlignTop)
-        newTab.setLayout(tabLayout)
-
-        # add tab to reference objects and return
-        self.tabWidget.addTab(newTab, "")
-        self.tabWidget.setTabText(self.tabWidget.indexOf(newTab), tabName)
-        return newTab
-
-    def addLabel(self, layout, text):
-        label = QtWidgets.QLabel()
-        label.setText(text)
-        label.setMinimumWidth(label.fontMetrics().width(label.text()))
-        layout.addWidget(label)
-
-    def addToParentLayout(self, layout, parent):
-        widget = QtWidgets.QWidget(parent)
-        widget.setLayout(layout)
-        parent.layout().addWidget(widget)
-
-    def addDoubleSpinBox(self, parent, name, x, y, maxResX=1920, maxResY=1080):
-        layout = QtWidgets.QHBoxLayout()
-
-        self.addLabel(layout, name)
-
-        xSpinBox = QtWidgets.QSpinBox()
-        ySpinBox = QtWidgets.QSpinBox()
-        xSpinBox.setMaximum(maxResX)
-        ySpinBox.setMaximum(maxResY)
-        xSpinBox.setValue(x)
-        ySpinBox.setValue(y)
-
-        layout.addWidget(xSpinBox)
-        layout.addWidget(ySpinBox)
-
-        self.addToParentLayout(layout, parent)
-
-        return xSpinBox, ySpinBox
-
-    def addTextInput(self, parent, text):
-        layout = QtWidgets.QHBoxLayout()
-
-        self.addLabel(layout, text)
-
-        # TODO: make the QLineEdit object expand width to fill parent
-        inputBox = QtWidgets.QLineEdit()
-        layout.addWidget(inputBox)
-
-        self.addToParentLayout(layout, parent)
-
-        return inputBox
-
-    def addComboBox(self, parent, text, items, default=None):
-        layout = QtWidgets.QHBoxLayout()
-
-        self.addLabel(layout, text)
-
-        # TODO: make the QComboBox object expand width to fill parent
-        comboBox = QtWidgets.QComboBox()
-        comboBox.addItems(items)
-        # TODO: figure out how to set default selection
-        layout.addWidget(comboBox)
-
-        self.addToParentLayout(layout, parent)
-
-        return comboBox
 
     def setAllObjectNames(self):
         for objName in dir(self):
@@ -152,8 +80,9 @@ class StatMaGICDockWidget(QtWidgets.QDockWidget):
     def display(self):
         message = (f"{self.nameInput.displayText()} wants to assess "
                    f"{self.mineralBox.currentText()} in the area defined by "
-                   f"{self.studyAreaBox.currentText()} at resolution "
-                   f"{self.xSpinBox.value()} x {self.ySpinBox.value()}.")
+                   f"{self.studyAreaBox.currentText()} at pixel size "
+                   f"{self.pixelSizeBox.value()} with buffer "
+                   f"{self.addBufferBox.value()}.")
         self.iface.messageBar().pushMessage(message)
 
     def greyscale(self):
